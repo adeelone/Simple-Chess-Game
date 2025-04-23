d1c1f8e9dab4af03daa08d960bcf60d5addbbb8c
Aden R <adeemra@gmail.com>
2025-04-23 10:50:38 -0600

    Update ChessBoard.java

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,33 +1,68 @@
 public class ChessBoard {
-
-    private static final int BOARD_SIZE = 8;
-    private char[][] board;
+    private Piece[][] board;
 
     public ChessBoard() {
-        board = new char[BOARD_SIZE][BOARD_SIZE];
-        initializeBoard();
+        board = new Piece[8][8];
+        setupPieces();
     }
 
-    private void initializeBoard() {
-        // Initialize the board with empty spaces
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                board[i][j] = '-';
-            }
+    private void setupPieces() {
+        // Place Rooks.
+        board[0][0] = new Rook(PieceColor.BLACK, new Position(0, 0));
+        board[0][7] = new Rook(PieceColor.BLACK, new Position(0, 7));
+        board[7][0] = new Rook(PieceColor.WHITE, new Position(7, 0));
+        board[7][7] = new Rook(PieceColor.WHITE, new Position(7, 7));
+
+        // Place Knights.
+        board[0][1] = new Knight(PieceColor.BLACK, new Position(0, 1));
+        board[0][6] = new Knight(PieceColor.BLACK, new Position(0, 6));
+        board[7][1] = new Knight(PieceColor.WHITE, new Position(7, 1));
+        board[7][6] = new Knight(PieceColor.WHITE, new Position(7, 6));
+
+        // Place Bishops.
+        board[0][2] = new Bishop(PieceColor.BLACK, new Position(0, 2));
+        board[0][5] = new Bishop(PieceColor.BLACK, new Position(0, 5));
+        board[7][2] = new Bishop(PieceColor.WHITE, new Position(7, 2));
+        board[7][5] = new Bishop(PieceColor.WHITE, new Position(7, 5));
+
+        // Place Queens.
+        board[0][3] = new Queen(PieceColor.BLACK, new Position(0, 3));
+        board[7][3] = new Queen(PieceColor.WHITE, new Position(7, 3));
+
+        // Place Kings.
+        board[0][4] = new King(PieceColor.BLACK, new Position(0, 4));
+        board[7][4] = new King(PieceColor.WHITE, new Position(7, 4));
+
+        // Place Pawns.
+        for (int i = 0; i < 8; i++) {
+            board[1][i] = new Pawn(PieceColor.BLACK, new Position(1, i));
+            board[6][i] = new Pawn(PieceColor.WHITE, new Position(6, i));
         }
     }
 
-    public void displayBoard() {
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
+    public boolean movePiece(Position start, Position end) {
+        Piece piece = board[start.getRow()][start.getCol()];
+        if (piece != null && piece.isValidMove(end, board)) {
+            // Move the piece.
+            board[end.getRow()][end.getCol()] = piece;
+            piece.setPosition(end);
+            board[start.getRow()][start.getCol()] = null;
+            return true;
         }
+        return false;
     }
 
-    public static void main(String[] args) {
-        ChessBoard chessBoard = new ChessBoard();
-        chessBoard.displayBoard();
+    public Piece[][] getBoard() {
+        return board;
     }
+
+    public Piece getPiece(int row, int col) {
+        return board[row][col];
+    }
+    public void setPiece(int row, int col, Piece piece) {
+        board[row][col] = piece;
+        if(piece != null) {
+            piece.setPosition(new Position(row, col));
+        }
+    } 
 }
