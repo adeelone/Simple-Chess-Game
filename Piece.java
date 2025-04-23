dab74add85ee4aec9f8d8f599c5863da3aaa12db
Aden R <adeemra@gmail.com>
2025-04-23 10:53:35 -0600

    Update Piece.java

---
+++ Piece.java
@@ -0,0 +1,28 @@
+public abstract class Piece {
+
+    protected PieceColor color;
+    protected Position position;
+
+    public Piece(PieceColor color, Position position) {
+        this.color = color;
+        this.position = position;
+    }
+
+    public PieceColor getColor() {
+        return color;
+    }
+
+    public void setColor(PieceColor color) {
+        this.color = color;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public abstract boolean isValidMove(Position newPosition, Piece[][] board);
+}
